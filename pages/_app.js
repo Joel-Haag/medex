41eebb92a3485e1888ff84d160c81ce4cfffd0f9
Joel Haag <joelohaag@gmail.com>
2022-10-19 12:45:39 +0200

    edit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+// import "../styles/globals.css";
+import "../styles/styles.css"
+import AOS from "aos";
+import "aos/dist/aos.css";
+import {useEffect} from "react";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    AOS.init({
+      // easing: "ease-out-cubic",
+      // once: true,
+      // offset: 50,
+      duration : 2000
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
